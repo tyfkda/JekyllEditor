415b7fc93807ae3a2d0d3303d7254b6138dd4dc3
tyfkda <tyfkda@gmail.com>
2016-03-04 11:32:27 +0900

    Use ngRoute to switch page

--- src/main.js
+++ src/main.js
@@ -4,9 +4,41 @@ class AppController {
   }
 }
 
+const TopPageComponent = {
+  controller: function() {
+  },
+  template: `
+    <div>Top!</div>
+    <a href="#/edit">edit</a>
+  `,
+}
+
+const EditPageComponent = {
+  controller: function() {
+  },
+  template: `
+    <div>Edit!</div>
+    <a href="#/">back</a>
+  `,
+}
+
 const APP = 'app'
-angular.module(APP, [])
+angular.module(APP, ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        template: '<top-page></top-page>',
+      })
+      .when('/edit', {
+        template: '<edit-page></edit-page>',
+      })
+      .otherwise({
+        redirectTo: '/',
+      })
+  }])
   .controller('AppController', [AppController])
+  .component('topPage', TopPageComponent)
+  .component('editPage', EditPageComponent)
 
 angular.element(document).ready(() => {
   angular.bootstrap(document, [APP])
