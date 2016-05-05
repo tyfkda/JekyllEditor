3c71c2050d24ccb70225c21e1fd77192b73290c2
tyfkda <tyfkda@gmail.com>
2016-05-05 18:45:28 +0900

    Replace date format '20xx-xx-xx' to '20xx/xx/xx'

--- src/app/util/util.ts
+++ src/app/util/util.ts
@@ -1,6 +1,7 @@
 export class Util {
   static parseDate(dateStr: string): Date {
-    const d = dateStr.replace(/-/g, '/')
+    const d = dateStr.replace(/(\d+)-(\d+)-(\d+)/,
+                              (_m, year, month, day) => `${year}/${month}/${day}`)
     return new Date(d)
   }
 }
