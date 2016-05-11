bb55ecfb2c4f96fe587e3ea597c4cb43bab658ba
tyfkda <tyfkda@gmail.com>
2016-05-12 08:57:33 +0900

    Remove semicolon

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {provide, enableProdMode} from '@angular/core';
-import {HTTP_PROVIDERS} from '@angular/http';
-import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common'
+import {bootstrap} from '@angular/platform-browser-dynamic'
+import {provide, enableProdMode} from '@angular/core'
+import {HTTP_PROVIDERS} from '@angular/http'
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated'
 
-import {Const} from './app/const';
-import {SeedApp} from './app/seed-app';
+import {Const} from './app/const'
+import {SeedApp} from './app/seed-app'
 
 if (!Config.isDebug) {
   enableProdMode()
@@ -16,4 +16,4 @@ bootstrap(SeedApp, [
   ROUTER_PROVIDERS,
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 ])
-.catch(err => console.error(err));
+.catch(err => console.error(err))
